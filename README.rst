17d1d0a4840b8f5a29ba645b8cc04e0fc06b3ad0
Daniel Woste <daniel.woste@useblocks.com>
2022-09-02 10:13:50 +0200

    Readme update

--- README.rst
+++ README.rst
@@ -1,7 +1,11 @@
-Dompteur
-========
+.. image:: https://raw.githubusercontent.com/useblocks/dompteur/main/docs/_static/dompteur_logo.png
+   :align: center
+   :width: 500px
+
 Dompteur is an installation and configuration tool for Sphinx, the documentation generator.
 
-.. hint:: This all this Pre-Alpha. Nothing to install and test yet.
+.. hint::
+
+   This all is Pre-Alpha. Nothing to install and test yet.
 
 Detailed docs under: https://dompteur.readthedocs.io/
--- setup.py
+++ setup.py
@@ -10,50 +10,29 @@ README_PATH = os.path.join(directory, 'README.rst')
 
 
 setup(
-    name='domnteur',
+    name='dompteur',
     version='0.0.1',
-    url='https://github.com/useblocks/domteur',
+    url='https://github.com/useblocks/dompteur',
     license='MIT',
     author='team useblocks',
     author_email='info@useblocks.com',
     description='A configuration tool for Sphinx projects',
     long_description=open(README_PATH, encoding='utf-8').read(),
     zip_safe=False,
-    packages=['sphinx_simplepdf',
-              'sphinx_simplepdf.builders',
-              'sphinx_simplepdf.directives',
-              'sphinx_simplepdf.themes/simplepdf_theme',
-              ],
-    package_data={'sphinx_simplepdf/themes/simplepdf_theme': [
-        'theme.conf',
-        '*.html',
-        'static/styles/*.css',
-        'static/js/*.js',
-        'static/fonts/*.*'
-    ]},
+    packages=[],
+    package_data={},
     include_package_data=True,
-    # See http://www.sphinx-doc.org/en/stable/theming.html#distribute-your-theme-as-a-python-package
     entry_points={
-        'sphinx.html_themes': [
-            'simplepdf_theme = sphinx_simplepdf.themes.simplepdf_theme',
-        ],
-        'sphinx.builders': [
-            'simplepdf = sphinx_simplepdf.builders.simplepdf'
-        ]
     },
-    install_requires=[
-        'sphinx',
-        'weasyprint',       # the used PDF builder
-        'libsass',           # needed to generate css on the fly
-        'beautifulsoup4',    # needed for HTML manipulations
-    ],
+    install_requires=[],
     classifiers=[
         'Framework :: Sphinx',
         'Framework :: Sphinx :: Theme',
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 1 - Planning',
         'License :: OSI Approved :: MIT License',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
         'Topic :: Documentation',
